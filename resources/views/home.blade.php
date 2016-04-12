fc13bfdf91e21d054f3b483bfe93f07340b4208d
yuri <yuridesnaijer@gmail.com>
2016-04-12 15:50:33 +0200

    added youtube api

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>sampleHub</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+
+    <div class="col-lg-6">
+        <div class="input-group">
+            <input id="youtubeSearchField" type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+                <button id="searchEventButton" class="btn btn-default" type="button">Go!</button>
+            </span>
+        </div><!-- /input-group -->
+    </div>
+
+    <div class="col-md-12">
+        <div id="player"></div>
+    </div>
+
+    <a href='//www.youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=i62Zjga8JOM' style='text-decoration:none;color:#03a730;'>
+        <img src='//www.youtubeinmp3.com/icon/download.png' style='vertical-align:middle;'> <strong>Download MP3</strong>
+    </a>
+</div>
+
+<script src="js/home.js" type="application/javascript"></script>
+</body>
+</html>
